312a00e789273c70b09c4591b8ae085c26409ab7
Anks <dubeyankur2833@gmail.com>
2024-02-25 01:20:48 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
 
       <div className="relative w-full h-full flex justify-center items-center">
         <img 
-        className="absolute z-[-1] w-full h-full object-cover object-center" 
+        className="absolute z-[-1] w-full h-full object-cover object-centercl" 
         src={"https://images.unsplash.com/photo-1493243350443-9e3048ce7288?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8c3Rvcm18ZW58MHwwfDB8fHww"} 
         alt="" 
         />
